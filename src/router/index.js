6a0bbeec01687a94b2f77be613da1984bc8663fd
zxfjd3g <258147149@qq.com>
2020-06-17 14:41:13 +0800

    登陆检查

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
+import stote from '@/store'
 
 // 声明使用vue插件
 Vue.use(VueRouter)
@@ -48,9 +49,8 @@ VueRouter.prototype.replace = function (location, onComplete, onAbort) {
   }
 }
 
-
-// 向外暴露路由器对象
-export default new VueRouter({
+// 创建路由器对象
+const router = new VueRouter({
   mode: 'history', // 不带#
   routes,
 
@@ -60,3 +60,49 @@ export default new VueRouter({
     return { x: 0, y: 0 }
   }
 })
+
+// 注册全局前置守卫
+/* 
+to: 目标路由对象
+from: 当前路由对象  对应的就$route
+next: 控制路由跳转的函数
+  不执行: 不放行, 不会跳转到目标路由
+  next(): 放行, 请求的路由组件才能显示
+  next(path): 强制跳转到指定路由去
+*/
+
+// 访问这些路由路径必须检查其已经登陆
+const checkPaths = ['/trade', '/pay', '/center' ]  // /paysuccess /center/myorder
+/* 
+功能: 只有登陆了, 才能查看交易/支付/个人中心界面
+*/
+router.beforeEach((to, from, next) => { // 监视的回调函数
+  console.log('全局beforeEach()', to, from)
+  // 得到目标路径
+  const targetPath = to.path  // /center/myorder
+  // 判断是目标路径是否需要检查
+  const needCheck = !!checkPaths.find(path => targetPath.indexOf(path)===0)
+  // 如果需要检查
+  // if (checkPaths.indexOf(targetPath)>=0)  // 请求路径可能是 /center/myorder
+  if (needCheck) {
+    // 已经登陆(state中的userInfo中有数据), 放行
+    const token = stote.state.user.userInfo.token
+    if (token) {
+      next()
+    } else {
+      // 如果没有登陆, 强制跳转到login界面
+      next('/login')
+    }
+  } else { // 如果是不需要检查的, 直接放行
+    // 放行
+    next()
+  }
+    
+
+  
+
+  
+})
+
+// 向外暴露路由器对象
+export default router

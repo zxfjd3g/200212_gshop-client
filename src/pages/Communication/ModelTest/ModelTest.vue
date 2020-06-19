f8b2b700aec5ae8863cb4c033a0211c7b93c4ff1
zxfjd3g <258147149@qq.com>
2020-06-19 10:48:21 +0800

    v-model

--- src/pages/Communication/ModelTest/ModelTest.vue
+++ src/pages/Communication/ModelTest/ModelTest.vue
@@ -1,10 +1,28 @@
 <template>
   <div>
     <h2>深入v-model</h2>
+    <!-- 
+      原生标签上的v-model的本质: 动态value属性和原生input事件监听
+    -->
+    <input type="text" v-model="name1">
+    <span>{{name1}}</span>
+    <br><br>
 
-    <input type="text">
-    <span>xxx</span>
-    <br>
+    <input type="text" :value="name2" @input="name2=$event.target.value">
+    <span>{{name2}}</span>
+    <br><br>
+
+    <!-- 
+      组件标签上的v-model的本质: 动态value属性和自定义input事件监听
+    -->
+    <CustomInput v-model="name3"/>
+    <span>{{name3}}</span>
+    <br><br>
+
+    <CustomInput :value="name4" @input="name4=$event"/>
+    <span>{{name4}}</span>
+    <br><br>
+
   </div>
 </template>
 
@@ -12,6 +30,16 @@
   import CustomInput from './CustomInput.vue'
   export default {
     name: 'ModelTest',
+
+    data () {
+      return {
+        name1: 'atguigu',
+        name2: 'atguigu22',
+        name3: 'atguigu33',
+        name4: 'atguigu44',
+      }
+    },
+
     components: {
       CustomInput
     }
